1653f6d1f26c8d8b5568f442509fdd09c3937956
ivar-tix <ivar@tix.is>
2023-02-10 14:28:44 +0100

    more cpp setup

---
+++ Project 1/includes/independent_output.cpp
@@ -0,0 +1,1 @@
+#include "independent_output.hpp"
--- Project 1/includes/utils.hpp
+++ Project 1/includes/utils.hpp
@@ -11,13 +11,13 @@ public:
 
     // given an input, output a hash of the output that is 0 >= max_size
     // h(c, m) = ((ac+b) mod p) mod m
-    static int hash(char input, int max_size){
-        int p = INT_MAX-1; // large prime number
-        int a = (p-1) * ((double)rand()) / RAND_MAX;
-        int b = (p-1) * ((double)rand()) / RAND_MAX;
+    static unsigned short hash(unsigned char input, int max_size){
+        unsigned int p = INT_MAX; // large prime number
+        unsigned int a = (p-1) * ((double)rand()) / RAND_MAX;
+        unsigned int b = (p-1) * ((double)rand()) / RAND_MAX;
         int c = (int)input;
 
-        return ((a*c + b) % p) % max_size;
+        return (unsigned short)((a*c + b) % p) % max_size;
     }
 };
 
