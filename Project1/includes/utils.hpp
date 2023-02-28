a9074f18a172e4229cd578ac2879fa819aa5f3fc
ivar-tix <ivar@tix.is>
2023-02-28 11:20:13 +0100

    hot stuff concurrent output

--- Project1/includes/utils.hpp
+++ Project1/includes/utils.hpp
@@ -1,5 +1,6 @@
 #include<cstdlib>
 #include <chrono>
+#include <time.h>
 
 // ensure only 15-bits
 #define RAND() (rand() & 0x7fff)
@@ -17,15 +18,18 @@ public:
 
     // given an input, output a hash of the output that is 0 >= max_size
     // h(c, m) = ((ac+b) mod p) mod m
-    static u64 hash(u64 input, int hash_bits){
-        srand((unsigned int)input);
+    u64 hash(u64 input, int hash_bits){
         u64 num = (((u64)RAND()<<48) ^ ((u64)RAND()<<35) ^ ((u64)RAND()<<22) ^
                    ((u64)RAND()<< 9) ^ ((u64)RAND()>> 4));
 
         return num & max_partition_hash(hash_bits);
     }
 
-    static u64 max_partition_hash(int hash_bits){
+    u64 max_partition_hash(int hash_bits){
+        return (0xffffffffffffffff << hash_bits) ^ 0xffffffffffffffff;
+    }
+
+    static u64 max_partition_hash_static(int hash_bits){
         return (0xffffffffffffffff << hash_bits) ^ 0xffffffffffffffff;
     }
 };
