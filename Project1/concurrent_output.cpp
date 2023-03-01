af66a782da07657096e407b872f11b21b818ffe0
ivar-tix <ivar@tix.is>
2023-03-01 23:12:43 +0100

    asdf

--- Project1/concurrent_output.cpp
+++ Project1/concurrent_output.cpp
@@ -113,7 +113,7 @@ int64_t run_experiment(int hash_bits, int num_threads, u64* &input)
     for(int i = 0; i <= max_partition_hash; i++){
         delete[] output_buffer[i];
 
-        std::cout << &output_buffer[i] << "\n"
+        std::cout << &output_buffer[i] << "\n";
 
         // if(sharedIndices[i] > highest){
         //     highest = sharedIndices[i];
