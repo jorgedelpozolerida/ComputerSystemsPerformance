554d1d108457d2ec7dd879b1f743b16b66ac1749
ivar-tix <ivar@tix.is>
2023-03-19 16:19:27 +0100

    shell script

--- Project1/independent_output.cpp
+++ Project1/independent_output.cpp
@@ -7,8 +7,13 @@
 #include <fstream>
 
 const int INPUT_SIZE = 2000000;
-const int MAX_NUM_THREADS = 32;
-const int MAX_HASH_BITS = 26;
+
+struct process_parameters{
+    int start; 
+    int end; 
+    int hash_bits; 
+    std::vector<std::vector<std::tuple<u64, u64>>> *buffer;
+};
 
 // u64 is defined in utils.hpp - it is an alias for usigned long long
 u64 *generate_input()
@@ -25,7 +30,7 @@ u64 *generate_input()
 }
 
 // we might want to pass the buffer by reference
-void process_partition(u64 *data, int start, int end, int hash_bits, std::vector<std::vector<std::tuple<u64, u64>>> buffer)
+void process_partition(u64 *data, int start, int end, int hash_bits, std::vector<std::vector<std::tuple<u64, u64>>> *buffer)
 {
     utils util_obj; 
 
@@ -34,29 +39,28 @@ void process_partition(u64 *data, int start, int end, int hash_bits, std::vector
         int hash = util_obj.hash(data[i], hash_bits);
         std::tuple<int, u64> t = std::make_tuple(hash, data[i]);
 
-        buffer.at(hash).push_back(t);
+        buffer->at(hash).push_back(t);
     }
 }
 
 int64_t run_experiment(int hash_bits, int num_threads, u64* &input)
 {
-    std::cout << "Threads: " << num_threads <<"\n";
-
     const int partition_size = INPUT_SIZE / num_threads;
 
     std::vector<std::thread> threads;
+    process_parameters* thread_parameters = new process_parameters[num_threads];
     std::vector<std::vector<std::vector<std::tuple<u64, u64>>>> thread_buffers;
 
     for (int i = 0; i < num_threads; ++i)
     {
-        std::vector<std::vector<std::tuple<u64, u64>>> output_buffer;
+        std::vector<std::vector<std::tuple<u64, u64>>>* output_buffer = new std::vector<std::vector<std::tuple<u64, u64>>>();
 
         // create "partitions"
         u64 max_partition_hash = utils::max_partition_hash_static(hash_bits);
         for (u64 i = 0; i <= max_partition_hash; i++)
         {
             std::vector<std::tuple<u64, u64>> partition_buffer;
-            output_buffer.push_back(partition_buffer);
+            output_buffer->push_back(partition_buffer);
         }
 
         // set which thread processes what part of the data
@@ -67,13 +71,22 @@ int64_t run_experiment(int hash_bits, int num_threads, u64* &input)
             end = INPUT_SIZE;
         }
 
-        // create the thread
-        std::thread thread(process_partition, input, start, end, hash_bits, output_buffer);
-        threads.push_back(std::move(thread));
+        process_parameters params;
+        params.buffer = output_buffer;
+        params.end = end;
+        params.start = start;
+        thread_parameters[i] = params;
     }
 
     // measuring the time
     auto start = std::chrono::steady_clock::now();
+    for (size_t i = 0; i < num_threads; i++)
+    {
+        // create the thread
+        std::thread thread(process_partition, input, thread_parameters[i].start, thread_parameters[i].end, hash_bits, std::ref(thread_parameters[i].buffer));
+        threads.push_back(std::move(thread));
+    }
+    
     for (std::thread &thread : threads)
     {
         thread.join();
@@ -82,37 +95,26 @@ int64_t run_experiment(int hash_bits, int num_threads, u64* &input)
     std::chrono::duration<double> elapsed_seconds = end - start;
     std::chrono::milliseconds elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(elapsed_seconds);
 
-    std::cout << "elapsed time: " << elapsed_ms.count() << "ms\n\n";
+    // clean-up
+    for (size_t i = 0; i < num_threads; i++)
+    {
+        delete thread_parameters[i].buffer;
+    }
+    delete[] thread_parameters;
 
     return elapsed_ms.count();
 }
 
-int main()
+int main(int argc, char* argv[])
 {
     srand(time(NULL));
-    u64 *input = generate_input();
+    u64* input = generate_input();
 
-    for (int experiment = 1; experiment <= 8; experiment += 1)
-    {
-        std::string filename = "./experiments/independent_output/experiment_" + std::to_string(experiment) + ".csv";
-        std::ofstream fout(filename);
+    int hash_bits = atoi(argv[1]);
+    int num_threads = atoi(argv[2]);
 
-        fout << "Threads;Hash_Bits;Running Time (ms)\n" << std::flush;
-
-        for (int hash_bits = 1; hash_bits <= MAX_HASH_BITS; hash_bits += 1) 
-        {
-            std::cout << " HASH BITS: " << hash_bits <<"\n";
-
-            for (int num_threads = 1; num_threads <= MAX_NUM_THREADS; num_threads *= 2) 
-            {
-                int64_t exp = run_experiment(hash_bits, num_threads, input);
-
-                fout << num_threads << ";" << hash_bits << ";" << exp << "\n" << std::flush;
-            }
-        }  
-
-        fout.close();
-    }
+    int64_t exp = run_experiment(hash_bits, num_threads, input);
+    std::cout << num_threads << ";" << hash_bits << ";" << exp << "\n";
 
     delete[] input;
 }
---
+++ Project1/python/.ipynb_checkpoints/generate_plots-checkpoint.py
@@ -0,0 +1,153 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+""" Script for generating plots for all experiments combined
+
+
+{Long Description of Script}
+"""
+__author__ = "Jorge del Pozo Lérida"
+__email__ =  "jorgedelpozolerida@gmail.com"
+__date__ =  "16/02/2023"
+
+
+import os
+import glob
+import argparse
+
+
+import logging                                                                      # NOQA E402
+import numpy as np                                                                  # NOQA E402
+import pandas as pd                                                                 # NOQA E402
+import matplotlib.pyplot as plt
+
+
+
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger(__name__)
+
+# Plotting config
+PLOT_MARKERS = ["|", "x", "2", "s", "P", "o"]
+PLOT_COLORS = ['r', 'g', 'b', 'm', 'c', 'y']
+plt.rcParams.update({'font.size': 16})
+plt.rcParams['lines.linewidth'] = 0.8
+
+# TO DO:
+# - Include memcpy for comparison (extreme upper bound)
+# - Change code if different number of hash bits
+
+def process_single_experiment_run(csv_path, args):
+    '''
+    Process whatever is on csv form experiment. 
+    Expected columns: Threads;Hash_Bits;Running Time (ms)
+    '''
+    input_size = int(args.input_size)
+    df = pd.read_csv(csv_path, sep=';', index_col=False).rename(
+        columns={ 
+                "Threads": "threads",
+                "Hash_Bits": "bits",
+                "Running Time (ms)": "miliseconds"
+                })
+    df['throughput'] = (input_size/ df['miliseconds']) / 1000 # to have millions/second 
+
+    return df
+
+
+def combine_experiment_trials(experiment_dir, args):
+    '''
+    Average over experiment runs
+    '''
+    all_files = glob.glob(os.path.join(experiment_dir, "*.csv"))
+    all_dfs = [process_single_experiment_run(os.path.join(experiment_dir, f), args) for f in all_files]
+    data_averaged = pd.concat(all_dfs).groupby(['bits', 'threads']).mean().reset_index()
+    return data_averaged
+
+
+def plot_experiment(ax, data_technique, title):
+    '''
+    Plot data for an experiment on given axis
+    '''
+    # Extract values for plot
+    thread_values = data_technique['threads'].unique()
+    labels = [f"Threads: {t}" for t in thread_values]
+    max_bits = data_technique['bits'].max()
+    ticks = np.arange(0, max_bits + 2, step=2)
+    assert len(PLOT_MARKERS) >= len(thread_values), \
+        "Need to specify more markers and colors for threads in PLOT_MARKERS PLOT_COLORS "
+    # Set axes values
+    ax.set_title(f"{title}")
+    ax.set_ylabel("Millions of Tuples per Second")
+    ax.set_xlabel("Hash Bits")
+    ax.set_xticks(ticks=ticks, labels=ticks)
+
+    # Plot
+    for j, th in enumerate(thread_values):
+        thread_data = data_technique.query("threads == @th")
+        ax.plot(thread_data['bits'], thread_data['throughput'], 
+            label=labels[j], 
+            marker=PLOT_MARKERS[j % len(PLOT_MARKERS)], 
+            color=PLOT_COLORS[j % len(PLOT_COLORS)])
+        ax.legend(labels,  prop={'size': 12})
+
+    return ax
+
+
+def main(args):
+
+    input_dir = args.in_dir
+    output_dir = args.out_dir
+
+    experiment_dirs = os.listdir(input_dir)
+    experiment_dirs.sort(reverse=True)
+    
+    # Get all data
+    data_techniques = [combine_experiment_trials(os.path.join(input_dir, subdir), args) for subdir in experiment_dirs ]
+    
+    # Big plot config
+    fig_all, axs_all = plt.subplots(1, len(experiment_dirs), figsize=(15,5), sharex=True)
+    fig_all.subplots_adjust(hspace=0)
+    y_lims = [0, np.ceil(max(  [ d['throughput'].max() for d in data_techniques] ))]
+
+    # Plot
+    for i, technique_dir  in enumerate(experiment_dirs):
+
+        data_technique = data_techniques[i]
+
+        # Single plot creation and saving
+        fig, ax = plt.subplots(figsize=(8,5))
+        ax = plot_experiment(ax,  data_technique, title=technique_dir)
+        fig.tight_layout()
+        fig.savefig(os.path.join(output_dir, f"{technique_dir}_plot.png" ))
+
+        # Subplot
+        axs_all[i] = plot_experiment(axs_all[i], data_technique, title=technique_dir)
+        axs_all[i].set_ylim(y_lims)
+        
+        _logger.info( f"Generated plots for: {technique_dir }")
+    
+    # Big plot saving
+    fig_all.tight_layout()
+    fig_all.savefig(os.path.join(output_dir, f"all_experiments_plot.png" ))
+
+
+
+def parse_args():
+    '''
+    Parses all script arguments.
+    '''
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--in_dir', type=str, default=None,
+                        help='Path to the input directory, expecting one folder per algorithm ')
+    parser.add_argument('--out_dir', type=str, default=None,
+                        help='Path to the output directory where plots are saved')
+    parser.add_argument('--input_size', type=str, default=None,
+                        help='Number of tuples/data size used in experiment')
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == '__main__':
+    args = parse_args()
+    main(args)
