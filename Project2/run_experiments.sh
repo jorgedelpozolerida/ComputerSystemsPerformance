93eb1ab165f19c1f1c0b735072134004375179e7
ivar-tix <ivar@tix.is>
2023-04-21 15:33:29 +0200

    asf

--- Project2/run_experiments.sh
+++ Project2/run_experiments.sh
@@ -1,11 +1,4 @@
-for i in {1..8}
-do
-    nvidia-smi --query-gpu=power.draw,temperature.gpu,memory.used --format=csv --loop-ms=100 > ./experiments/tensorflow/experiment-$i.csv
-    & python ./training_pytorch.py
-done
-
-for i in {1..8}
-do
-    nvidia-smi --query-gpu=power.draw,temperature.gpu,memory.used --format=csv --loop-ms=100 > ./experiments/pytorch/experiment-$i.csv
-    & python ./training_tensorflow.py
-done
+nvidia-smi --query-gpu=power.draw,temperature.gpu,memory.used --format=csv --loop-ms=100 > ./experiments/pytorch/experiment-test.csv &
+serverPID=$!
+python ./src/training_tensorflow.py --dataset=CIFAR10 --resnet_size="resnet50"
+kill serverPID
