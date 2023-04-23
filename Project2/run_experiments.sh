ca002efd47444d4c7c63144199088784f840e308
ivar-tix <ivar@tix.is>
2023-04-23 14:56:54 +0200

    latest changes

--- Project2/run_experiments.sh
+++ Project2/run_experiments.sh
@@ -1,4 +1,4 @@
-nvidia-smi --query-gpu=power.draw,temperature.gpu,memory.used --format=csv --loop-ms=100 > ./experiments/pytorch/experiment-test.csv &
+nvidia-smi --query-gpu=power.draw,temperature.gpu,memory.used --format=csv --loop-ms=1000 > ./experiments/pytorch/experiment-test.csv &
 serverPID=$!
 python ./src/training_tensorflow.py --dataset=CIFAR10 --resnet_size="resnet50"
 kill serverPID
