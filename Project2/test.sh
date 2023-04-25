f3bbf1d60e5bc356a2ad3a934d77cf291a3faeee
ivar-tix <ivar@tix.is>
2023-04-25 10:14:54 +0200

    added a test job

--- Project2/test.sh
+++
@@ -1,6 +0,0 @@
-nvidia-smi --query-gpu=power.draw,temperature.gpu,memory.used --format=csv --loop-ms=1000 > ./experiments/pytorch/experiment-test.csv &
-serverPID=$!
-date +%Y-%m-%d_%H-%M-%S.%N > ./log
-python ./src/training_tensorflow.py --dataset=CIFAR10 --resnet_size="resnet50" > ./log
-date +%Y-%m-%d_%H-%M-%S.%N > ./log
-kill serverPID
