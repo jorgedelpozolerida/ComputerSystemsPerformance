dac018dc80652564c04983c073606840d8f60866
ivar-tix <ivar@tix.is>
2023-04-27 17:55:49 +0200

    not only lazy, but also dumb

--- Project2/src/training_tensorflow.py
+++ Project2/src/training_tensorflow.py
@@ -27,7 +27,7 @@ DATAIN_PATH = os.path.join( os.path.abspath(os.path.join(THISFILE_PATH, os.pardi
 EXPORT_PATH = os.path.join( os.path.abspath(os.path.join(THISFILE_PATH, os.pardir, os.pardir)))
 
 def main(args):
-    file_name  = f"run-${args.run}_device-${args.device}_epoch-${args.epochs}_batchsize-${args.batch_size}_framework-tensorflow_dataset-${args.dataset}_model-${args.model}_MODEL.csv"
+    file_name  = f"run-${args.run}_device-${args.device}_epoch-${args.epochs}_batchsize-${args.batch_size}_framework-tensorflow_dataset-${args.dataset}_model-${args.resnet_size}_MODEL.csv"
     csv_path = EXPORT_PATH = os.path.join(EXPORT_PATH, 'experiments', args.device, 'tensorflow', file_name)
 
     # Print the parsed arguments
