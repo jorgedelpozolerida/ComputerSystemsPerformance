a5432069fd9edbddfdb842cbd57d71ada57015c5
ivar-tix <ivar@tix.is>
2023-05-02 14:35:55 +0200

    changing test batch size and mapping function

--- Project2/src/training_tensorflow.py
+++ Project2/src/training_tensorflow.py
@@ -28,6 +28,8 @@ EXPORT_PATH = os.path.join( os.path.abspath(os.path.join(THISFILE_PATH, os.pardi
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 
+tuple_map = lambda x, y: (x, y)
+
 def main(args):
     file_name  = f"run{args.run}-{args.device}-epoch{args.epochs}-batchsize{args.batch_size}-tensorflow-{args.dataset}-{args.resnet_size}_MODEL.csv"
     csv_path = os.path.join(EXPORT_PATH, 'experiments', args.device, 'tensorflow', file_name)
@@ -56,7 +58,7 @@ def main(args):
             print("GPU: ",physical_devices)
         else:
             print(tf.config.list_physical_devices())
-            raise ValueError("There is no gpu available, please use cpu")
+            #raise ValueError("There is no gpu available, please use cpu")
 
     # Load the dataset
     (x_train, y_train), (x_test, y_test) = get_dataset(args.dataset)
@@ -87,7 +89,7 @@ def main(args):
         # train_dataset.shuffle(buffer_size=1024)
         train_dataset
         .batch(batch_size)
-        .map(lambda x, y: (x, y), num_parallel_calls=tf.data.experimental.AUTOTUNE)
+        .map(tuple_map, num_parallel_calls=tf.data.experimental.AUTOTUNE)
         .prefetch(tf.data.experimental.AUTOTUNE)
     )
 
