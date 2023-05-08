216c858f77a22e1ef95c9ca6756e9c9298a48f0d
jorgedelpozolerida <jorgedelpozolerida@gmail.com>
2023-05-08 20:46:41 +0200

    preprocessed data merged and saved from both framweroks, scripts reorganized

--- Project2/src/generate_plots.py
+++ Project2/src/generate_plots.py
@@ -44,179 +44,6 @@ TODO:
 '''
 
 
-def get_modelpath_from_energypath(path):
-    '''
-    Creates full path for model path based on energy path
-    '''
-    newpath = path.replace("-ENERGY", "_MODEL")
-    if os.path.exists(newpath):
-        return newpath
-    else:
-        return  None
-
-def get_experiments_overview(exp_dir, framework):
-    '''
-    Gets an overview of available data with necessary paths
-    '''
-    csv_list = [os.path.join(exp_dir, framework, f) for f in os.listdir(os.path.join(exp_dir, framework)) if f.endswith('.csv')]
-
-    df_list = []
-    
-    # Parse 
-    for csv_fullpath in csv_list:
-        if csv_fullpath.endswith("ENERGY.csv"):
-            filename = os.path.basename(csv_fullpath)
-            file_parts = re.split('-', filename)
-            sub_df = {
-            "run": re.findall(r'\d+', file_parts[0])[0],
-            "device": file_parts[1],
-            "max_epoch": re.findall(r'\d+', file_parts[2])[0],
-            "batch_size":  re.findall(r'\d+', file_parts[3])[0],
-            "framework": file_parts[4],
-            "dataset": file_parts[5],
-            "model": file_parts[6],
-            "energy_filepath": csv_fullpath,
-            "model_filepath": get_modelpath_from_energypath(csv_fullpath)
-            }
-            df_list.append(sub_df)
-            
-    df = pd.DataFrame.from_records(df_list)
-                
-    return df
-
-
-def read_energy_csv(csv_path):
-    '''
-    Reads energy data an processes it into clean format
-    '''
-    energy_data = pd.read_csv(csv_path)
-    
-    # rename cols
-    new_column_names = {
-    "timestamp": "timestamp",
-    ' power.draw [W]': 'power',
-    ' temperature.gpu': 'temp',
-    ' memory.used [MiB]': 'mem_used',
-    ' utilization.gpu [%]': 'gpu_util',
-    ' utilization.memory [%]': 'mem_util'
-    }
-    energy_data.rename(columns=new_column_names, inplace=True)
-
-    # Process data into nice format and datatype
-    # time
-    energy_data['timestamp'] = pd.to_datetime(energy_data['timestamp'])
-    energy_data['time_sec'] = (energy_data['timestamp'] - energy_data['timestamp'][0]).dt.total_seconds()
-    # power
-    energy_data['power'] = energy_data['power'].str.strip('W').astype(float)  
-    # memory
-    energy_data['mem_used'] = energy_data['mem_used'].str.strip('MiB').astype(float)
-    # percentages
-    energy_data['gpu_util'] = energy_data['gpu_util'].str.strip('%').astype(int)
-    energy_data['mem_util'] = energy_data['mem_util'].str.strip('%').astype(int)
-
-    return energy_data
-
-def read_model_csv(csv_path, framework, timestamp_0):
-    '''
-    Reads model data an processes it into clean format
-    '''
-    # if framework == 'pytorch':
-    #     # read with error in it
-    #     header  = ["epoch","precision","recall","accuracy", "f1", "error", "timestamp"] 
-    #     model_data = pd.read_csv(csv_path, skiprows=[0], sep=';|;;', names=header, engine='python')
-    #     model_data.drop("error", axis=1, inplace=True)
-    # else :
-    #     continue
-    #     # read normally
-    
-    model_data = pd.read_csv(csv_path, sep=";")
-    
-    # Process data into nice format and datatype
-    # time
-    model_data['timestamp'] = pd.to_datetime(model_data['timestamp'])
-    model_data['time_sec'] = (model_data['timestamp'] - timestamp_0).dt.total_seconds()
-    # round evaluation metrics
-    model_data['precision'] = model_data['precision'].round(3)
-    model_data['recall'] = model_data['recall'].round(3)
-    model_data['f1'] = model_data['f1'].round(3)
-    
-    return model_data
-
-def get_allexperiments_data(exp_dir, framework):
-    '''
-    Generates necessary data for framework experiments
-    '''
-    
-    # Generate overview of data for framwork and save
-    df_overview = get_experiments_overview(exp_dir, framework)
-    save_dir = utils.ensure_dir(os.path.join(PROJECT2_PATH,"dataout", framework ))
-
-    
-    
-    # Extract tabular data for energy and model separately
-
-    energy_data = []
-    model_data = []
-    
-    for index, row in tqdm(df_overview.iterrows(), desc=f"Reading csv data from {framework} experiments", total=df_overview.shape[0] ):
-        # read data for that experiment
-        energy_data_temp = read_energy_csv(row['energy_filepath'])
-        model_data_temp = read_model_csv(row['model_filepath'], framework,
-                                         timestamp_0 = energy_data_temp['timestamp'].min() # needed to coordinate both time series
-                                         )
-
-        # Assign bins to time in energy data 
-        bins = model_data_temp['time_sec'].unique().tolist()
-        bins = [-1] + bins
-        bins[-1] = bins[-1] + 1
-        labels = [int(i) + 1 for i in model_data_temp['epoch'].unique().tolist()]
-
-
-        energy_data_temp['epoch_number'] =  pd.cut( energy_data_temp['time_sec'], bins=bins, labels=labels)
-
-        
-        # remove data not inside any epoch timeframe
-        energy_data_temp = energy_data_temp[ ~ energy_data_temp['epoch_number'].isna()]
-        
-        # convert all epochs to same scale
-        model_data_temp['epoch'] = model_data_temp['epoch'].astype(int) + 1
-
-
-        # Add id columns and append to list with all data
-        row_temp = row.drop(['energy_filepath', 'model_filepath'])
-
-        model_data.append(model_data_temp.assign(**row_temp))
-        energy_data.append(energy_data_temp.assign(**row_temp))
-
-
-    model_data = pd.concat(model_data)
-    energy_data = pd.concat(energy_data)
-    
-    # combine data per runs and per batches and average across runs
-    columns_to_avg = [
-        # energy cols
-        'power', 'temp', 'mem_used', 'gpu_util', 'mem_util',
-        # model cols
-        "precision", "recall", "accuracy", "f1"
-                      ]
-    energy_data_combined = pd.merge(energy_data,
-                                    model_data,
-                                    left_on=['epoch_number', 'run', 'device', 'max_epoch', 'batch_size', 'framework','dataset', 'model'],
-                                    right_on = ['epoch', 'run', 'device', 'max_epoch', 'batch_size', 'framework','dataset', 'model'],
-                                    how='left')
-    energydata_averagedperepoch = energy_data_combined.groupby(['epoch_number', 'run', 'device', 'max_epoch', 'batch_size', 'framework', 'dataset', 'model'])[columns_to_avg].agg('mean').reset_index()
-    energydata_averagedperepoch_averagedthroughruns = energydata_averagedperepoch.groupby(['epoch_number', 'device', 'max_epoch', 'batch_size', 'framework', 'dataset', 'model'])[columns_to_avg].agg('mean').round(3).reset_index()
-
-
-    # Save data
-    _logger.info(f"Saving overview for {framework}")
-    df_overview.drop(['energy_filepath', 'model_filepath'], axis=1).to_csv(os.path.join(save_dir, f"data_overview_{framework}.csv"), 
-                       index=False)
-    energydata_averagedperepoch_averagedthroughruns.to_csv(os.path.join(save_dir, f"cleandata_perepoch_{framework}.csv"), index=False)
-
-    return df_overview, model_data, energy_data, energydata_averagedperepoch_averagedthroughruns
-
-
 def plot_energyvar_timeseries(fig, ax, energy_data, model_data, levels_values, var_to_plot,x_var="time_sec", title=""):
     '''
     Plots some var_to_plot 'y' through time from energy data for some combination of levels values (id).
@@ -312,20 +139,33 @@ def plot_singleexperiment_epochs( levels_values, combined_data, framework,
         fig.savefig(os.path.join(save_dir, f"singleexperiment_{x_var}_" + "_".join([f"{k}={v}" for k, v in levels_values.items()])+ ".png"),  bbox_inches='tight')
 
 def main(args):
-
-
-    exp_dir = os.path.join(PROJECT2_PATH, "experiments", args.device)
     
-    # PYTORCH -----------------------------------------------------------------
-    # Load experiment data
-    df_overview_pytorch, model_data_pytorch, energy_data_pytorch, combineddata_perepoch_averaged = get_allexperiments_data(exp_dir, "pytorch")
+    # LOAD DATA ------------------------------------------------------------
+    # Pytorch 
+    
+    df_overview_pytorch, model_data_pytorch, energy_data_pytorch, combined_data_pytorch = utils.get_allexperiments_data("pytorch", device=args.device)
     print("PYTORCH\n",
           f"Number of experiments: {df_overview_pytorch.shape}\n", 
           f"Size of model data: {model_data_pytorch.shape}\n", 
           f"Size of energy data: {energy_data_pytorch.shape}\n",
-          f"Size of combined data per epoch (averaged through runs): {combineddata_perepoch_averaged.shape}",
+          f"Size of combined data per epoch (averaged through runs): {combined_data_pytorch.shape}",
+          )
+    # Tensorflow ------------------------------------------------------------
+    df_overview_tensorflow, model_data_tensorflow, energy_data_tensorflow, combined_data_tensorflow = utils.get_allexperiments_data( "tensorflow", device=args.device)
+    print("TENSORFLOW\n",
+          f"Number of experiments: {df_overview_tensorflow.shape}\n", 
+          f"Size of model data: {model_data_tensorflow.shape}\n", 
+          f"Size of energy data: {energy_data_tensorflow.shape}\n",
+          f"Size of combined data per epoch (averaged through runs): {combined_data_tensorflow.shape}",
 
           )
+    
+    # All data merged together and saved 
+    combined_data_pytorch['framework'] = 'pytorch'
+    combined_data_tensorflow['framework'] = 'tensorflow'
+    
+    merged_df = pd.concat([combined_data_pytorch, combined_data_tensorflow], ignore_index=True)
+    merged_df.to_csv(os.path.join(PROJECT2_PATH, "dataout", "all_data_processed.csv"))
 
 
     # # 1 - Single experiment
@@ -353,29 +193,7 @@ def main(args):
     # 2 - Total energy consumption of training between the frameworks per Model and Batch size
     # 3 - Maximum energy spike of the same variance
     # 4 - Growth of energy usage + gpu utilization for increase in batch size
-    # 5 - Mean gpu utilization between frameworks  
-
-
-    # TENSORFLOW -----------------------------------------------------------------
-    df_overview_pytorch, model_data_pytorch, energy_data_pytorch, combineddata_perepoch_averaged = get_allexperiments_data(exp_dir, "tensorflow")
-    print("TENSORFLOW\n",
-          f"Number of experiments: {df_overview_pytorch.shape}\n", 
-          f"Size of model data: {model_data_pytorch.shape}\n", 
-          f"Size of energy data: {energy_data_pytorch.shape}\n",
-          f"Size of combined data per epoch (averaged through runs): {combineddata_perepoch_averaged.shape}",
-
-          )
-    # 1 - Single experiment ---------------------------------------------
-    # 2 - Total energy consumption of training between the frameworks per Model and Batch size
-    # 3 - Maximum energy spike of the same variance
-    # 4 - Growth of energy usage + gpu utilization for increase in batch size
-    # 5 - Mean gpu utilization between frameworks  
-    
-
-
-
-
-    return
+    # 5 - Mean gpu utilization between frameworks
 
 
 def parse_args():
--- Project2/src/utils.py
+++ Project2/src/utils.py
@@ -11,12 +11,18 @@ from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_sc
 
 import numpy as np                                                                  # NOQA E402
 import datetime
+import pandas as pd
+import logging
+from tqdm import tqdm
+import re
 
 # constant which denotes where to cache the data
 THISFILE_PATH = os.path.abspath(__file__)
+PROJECT2_PATH =  os.path.abspath(os.path.join(THISFILE_PATH, os.pardir, os.pardir))
 DATAIN_PATH = os.path.join( os.path.abspath(os.path.join(THISFILE_PATH, os.pardir, os.pardir)), 'datain')
-EXPERIMENTS_PATH = os.path.join( os.path.abspath(os.path.join(THISFILE_PATH, os.pardir, os.pardir)), 'experiments')
 
+logging.basicConfig(level=logging.INFO)
+_logger = logging.getLogger(__name__)
 
 
 ###########################################
@@ -136,3 +142,182 @@ def ensure_dir(directory):
         print(f"Created the following dir: {directory}")
     
     return directory
+
+
+###########################################
+# ------ Experiment data loading ---------
+###########################################
+
+
+
+def get_modelpath_from_energypath(path):
+    '''
+    Creates full path for model path based on energy path
+    '''
+    newpath = path.replace("-ENERGY", "_MODEL")
+    if os.path.exists(newpath):
+        return newpath
+    else:
+        return  None
+
+def get_experiments_overview(exp_dir, framework):
+    '''
+    Gets an overview of available data with necessary paths
+    '''
+    csv_list = [os.path.join(exp_dir, framework, f) for f in os.listdir(os.path.join(exp_dir, framework)) if f.endswith('.csv')]
+
+    df_list = []
+    
+    # Parse 
+    for csv_fullpath in csv_list:
+        if csv_fullpath.endswith("ENERGY.csv"):
+            filename = os.path.basename(csv_fullpath)
+            file_parts = re.split('-', filename)
+            sub_df = {
+            "run": re.findall(r'\d+', file_parts[0])[0],
+            "device": file_parts[1],
+            "max_epoch": re.findall(r'\d+', file_parts[2])[0],
+            "batch_size":  re.findall(r'\d+', file_parts[3])[0],
+            "framework": file_parts[4],
+            "dataset": file_parts[5],
+            "model": file_parts[6],
+            "energy_filepath": csv_fullpath,
+            "model_filepath": get_modelpath_from_energypath(csv_fullpath)
+            }
+            df_list.append(sub_df)
+            
+    df = pd.DataFrame.from_records(df_list)
+                
+    return df
+
+
+def read_energy_csv(csv_path):
+    '''
+    Reads energy data an processes it into clean format
+    '''
+    energy_data = pd.read_csv(csv_path)
+    
+    # rename cols
+    new_column_names = {
+    "timestamp": "timestamp",
+    ' power.draw [W]': 'power',
+    ' temperature.gpu': 'temp',
+    ' memory.used [MiB]': 'mem_used',
+    ' utilization.gpu [%]': 'gpu_util',
+    ' utilization.memory [%]': 'mem_util'
+    }
+    energy_data.rename(columns=new_column_names, inplace=True)
+
+    # Process data into nice format and datatype
+    # time
+    energy_data['timestamp'] = pd.to_datetime(energy_data['timestamp'])
+    energy_data['time_sec'] = (energy_data['timestamp'] - energy_data['timestamp'][0]).dt.total_seconds()
+    # power
+    energy_data['power'] = energy_data['power'].str.strip('W').astype(float)  
+    # memory
+    energy_data['mem_used'] = energy_data['mem_used'].str.strip('MiB').astype(float)
+    # percentages
+    energy_data['gpu_util'] = energy_data['gpu_util'].str.strip('%').astype(int)
+    energy_data['mem_util'] = energy_data['mem_util'].str.strip('%').astype(int)
+
+    return energy_data
+
+def read_model_csv(csv_path, framework, timestamp_0):
+    '''
+    Reads model data an processes it into clean format
+    '''
+    # if framework == 'pytorch':
+    #     # read with error in it
+    #     header  = ["epoch","precision","recall","accuracy", "f1", "error", "timestamp"] 
+    #     model_data = pd.read_csv(csv_path, skiprows=[0], sep=';|;;', names=header, engine='python')
+    #     model_data.drop("error", axis=1, inplace=True)
+    # else :
+    #     continue
+    #     # read normally
+    
+    model_data = pd.read_csv(csv_path, sep=";")
+    
+    # Process data into nice format and datatype
+    # time
+    model_data['timestamp'] = pd.to_datetime(model_data['timestamp'])
+    model_data['time_sec'] = (model_data['timestamp'] - timestamp_0).dt.total_seconds()
+    # round evaluation metrics
+    model_data['precision'] = model_data['precision'].round(3)
+    model_data['recall'] = model_data['recall'].round(3)
+    model_data['f1'] = model_data['f1'].round(3)
+    
+    return model_data
+
+def get_allexperiments_data(framework, device="gpu"):
+    '''
+    Generates necessary data for framework experiments
+    '''
+    exp_dir = os.path.join(PROJECT2_PATH,  "experiments", device)
+    # Generate overview of data for framwork and save
+    df_overview = get_experiments_overview(exp_dir, framework)
+    save_dir = ensure_dir(os.path.join(PROJECT2_PATH,"dataout", framework ))
+
+    
+    
+    # Extract tabular data for energy and model separately
+
+    energy_data = []
+    model_data = []
+    
+    for index, row in tqdm(df_overview.iterrows(), desc=f"Reading csv data from {framework} experiments", total=df_overview.shape[0] ):
+        # read data for that experiment
+        energy_data_temp = read_energy_csv(row['energy_filepath'])
+        model_data_temp = read_model_csv(row['model_filepath'], framework,
+                                         timestamp_0 = energy_data_temp['timestamp'].min() # needed to coordinate both time series
+                                         )
+
+        # Assign bins to time in energy data 
+        bins = model_data_temp['time_sec'].unique().tolist()
+        bins = [-1] + bins
+        bins[-1] = bins[-1]
+        labels = [int(i) + 1 for i in model_data_temp['epoch'].unique().tolist()]
+
+
+        energy_data_temp['epoch_number'] =  pd.cut( energy_data_temp['time_sec'], bins=bins, labels=labels)
+
+        
+        # remove data not inside any epoch timeframe
+        energy_data_temp = energy_data_temp[ ~ energy_data_temp['epoch_number'].isna()]
+        
+        # convert all epochs to same scale
+        model_data_temp['epoch'] = model_data_temp['epoch'].astype(int) + 1
+
+
+        # Add id columns and append to list with all data
+        row_temp = row.drop(['energy_filepath', 'model_filepath'])
+
+        model_data.append(model_data_temp.assign(**row_temp))
+        energy_data.append(energy_data_temp.assign(**row_temp))
+
+
+    model_data = pd.concat(model_data)
+    energy_data = pd.concat(energy_data)
+    
+    # combine data per runs and per batches and average across runs
+    columns_to_avg = [
+        # energy cols
+        'power', 'temp', 'mem_used', 'gpu_util', 'mem_util',
+        # model cols
+        "precision", "recall", "accuracy", "f1"
+                      ]
+    energy_data_combined = pd.merge(energy_data,
+                                    model_data,
+                                    left_on=['epoch_number', 'run', 'device', 'max_epoch', 'batch_size', 'framework','dataset', 'model'],
+                                    right_on = ['epoch', 'run', 'device', 'max_epoch', 'batch_size', 'framework','dataset', 'model'],
+                                    how='left')
+    energydata_averagedperepoch = energy_data_combined.groupby(['epoch_number', 'run', 'device', 'max_epoch', 'batch_size', 'framework', 'dataset', 'model'])[columns_to_avg].agg('mean').reset_index()
+    energydata_averagedperepoch_averagedthroughruns = energydata_averagedperepoch.groupby(['epoch_number', 'device', 'max_epoch', 'batch_size', 'framework', 'dataset', 'model'])[columns_to_avg].agg('mean').round(3).reset_index()
+
+
+    # Save data
+    _logger.info(f"Saving overview for {framework}")
+    df_overview.drop(['energy_filepath', 'model_filepath'], axis=1).to_csv(os.path.join(save_dir, f"data_overview_{framework}.csv"), 
+                       index=False)
+    energydata_averagedperepoch_averagedthroughruns.to_csv(os.path.join(save_dir, f"cleandata_perepoch_{framework}.csv"), index=False)
+
+    return df_overview, model_data, energy_data, energydata_averagedperepoch_averagedthroughruns
